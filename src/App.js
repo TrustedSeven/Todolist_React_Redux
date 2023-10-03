f432bc858b0d03b2c90c065f62c815a3f3305689
TrustedSeven <castaneda.chistian0215@gmail.com>
2023-10-03 07:24:14 -0500

    initial complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AddTodo } from './components/AddTodo';
+import { TodoLists } from './components/TodoLists';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          To-do App
-        </a>
-      </header>
+    <div className="container p-4 mt-2">
+      <h2>Todo Application</h2>
+      <AddTodo />
+      <TodoLists />
     </div>
   );
 }
